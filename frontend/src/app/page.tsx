1b6a371707b7932f75ae90c61e7d52ecc6c06feb
Naveen M K <naveen521kk@gmail.com>
2024-03-16 23:57:22 +0530

    updates

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center container space-y-4 my-5 lg:my-0">
       <div className="w-full space-y-2">
-        <Label htmlFor="essay" className="font-semibold text-2xl text-center">
+        {/* <Label htmlFor="essay" className="font-semibold text-2xl text-center">
           Essay Validator
-        </Label>
+        </Label> */}
+        <div className="space-y-2">
+          <h1 className="text-3xl font-bold tracking-tight">
+            Paste your essay
+          </h1>
+          <p className="text-gray-500 dark:text-gray-400">
+            Get detailed feedback on your writing.
+          </p>
+        </div>
         <Textarea
           id="essay"
-          className="min-h-[70vh] text-xl"
-          placeholder="start typing your essay here..."
+          className="min-h-[70vh] text-lg"
+          placeholder="Paste your essay here."
         />
       </div>
       <div className="w-full flex flex-row space-x-3">
---
+++ frontend/src/app/reports/progress-bar-circle.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+interface ProgressBarCircleProps {
+  progress: number;
+}
+
+const ProgressBarCircle: React.FC<ProgressBarCircleProps> = ({ progress }) => {
+  const circumference = 2 * Math.PI * 45; // Assuming the radius of the circle is 45
+
+  const offset = circumference - (progress / 100) * circumference;
+
+  return (
+    <div className="relative mx-6">
+      <svg
+        className="w-full -rotate-90"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      >
+        <circle
+          className="text-primary-foreground opacity-20"
+          cx="50"
+          cy="50"
+          r="45"
+          strokeWidth="6"
+          fill="transparent"
+        />
+        <circle
+          className="text-primary"
+          cx="50"
+          cy="50"
+          r="45"
+          strokeWidth="6"
+          fill="transparent"
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+        />
+      </svg>
+      <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-lg font-bold">
+        {progress}%
+      </span>
+    </div>
+  );
+};
+
+export { ProgressBarCircle };
